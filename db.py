5dfc89a1e575bb15469243dd4938e0e89bceedef
Krunaldhanani005 <23aikru005@ldce.ac.in>
2025-07-17 15:25:42 +0530

    Upload Project images

--- db.py
+++ db.py
@@ -5,7 +5,7 @@ def get_connection():
         host="localhost",
         user="root",
         password="root@12345",
-        database="sweet_shop2" # Ensure this matches your database name
+        database="sweet_shop" # Ensure this matches your database name
     )
 
 def get_all_sweets(query='', sort_by='id'):
